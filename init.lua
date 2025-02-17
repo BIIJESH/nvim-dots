47c21e06801ba28e64464939f5d5cd17074ac629
BIIJESH <BIIJESH@proton.me>
2025-02-17 16:09:28 +0545

    removed extralsp

--- init.lua
+++ init.lua
@@ -2,23 +2,3 @@ require("config.options")
 require("config.keymaps")
 require("config.autocmds")
 require("config.lazy")
-local client = vim.lsp.start_client {
-  name = "educationalsp",
-  cmd = { "/home/papa/educationalsp/main" },
-  on_attach = function(client, bufnr)
-    print("LSP server attached!")
-  end,
-}
-
-if not client then
-  vim.notify("LSP client failed to start!")
-  return
-end
-
--- Autocmd to attach the LSP client to markdown files
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "markdown",
-  callback = function()
-    vim.lsp.buf_attach_client(0, client)
-  end,
-})
