f60fb2db6d9308258fc746ad666bc59b56d59eb0
BIIJESH <BIIJESH@proton.me>
2025-05-08 17:49:48 +0545

    changes

--- ftplugin/gopls.lua
+++
@@ -1,7 +0,0 @@
-vim.lsp.config.gopls = {
-  gopls = {
-    cmd = { "gopls" },
-    filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  },
-}
-vim.lsp.enable("gopls")
--- ftplugin/python.lua
+++
@@ -1,5 +0,0 @@
-vim.lsp.start({
-  name = 'pyright-langserver',
-  cmd = {'pyright-langserver', '--stdio'},
-  root_dir = vim.fs.dirname(vim.fs.find({'__init__.py'}, { upward = true })[1]),
-})
